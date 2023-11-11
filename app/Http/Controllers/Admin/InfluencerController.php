2594e79e0d84d0a333d0468eea07c07881adfa41
islamatiyat01 <islamatiyat50@yahoo.com>
2023-11-12 00:23:19 +0300

    influencer

--- app/Http/Controllers/Admin/InfluencerController.php
+++ app/Http/Controllers/Admin/InfluencerController.php
@@ -19,29 +19,61 @@ class InfluencerController extends Controller
     }
    public function store(InfluencerFormRequest $request)
     {
+        $validatedData = $request->validated();
+        $influencer = new Influencer;
+        $influencer->name = $validatedData['name'];
+        $influencer->email = $validatedData['email'];
+        $influencer->phone =$validatedData['phone'];
 
-        $validatedData = $request->validated(); $influencer = new Influencer;
-$influencer->name = $validatedData['name']; $influencer->email =
-$validatedData['email']; $influencer->phone =$validatedData['phone'];
-$influencer->description = $validatedData['description']; $uploadPath =
-'uploads/influencer/'; if ($request->hasFile('image')) { $file =
-$request->file(('image')); $ext = $file->getClientOriginalExtension(); $filename
-= time() . '.' . $ext; //current timestamp $file->move('uploads/influencer/',
-$filename); $influencer->image = $uploadPath . $filename; } $influencer->save();
-return redirect('/admin/influencer')->with('message', 'influencer Added
-Successfully'); } public function edit(Influencer $influencer) { return
-view('admin.influencer.edit', compact('influencer')); } public function
-update(InfluencerFormRequest $request, $influencer) { //dd($request->all());
-$influencer = Influencer::findOrFail($influencer); //retrieve and work with
-specific records from the database for further manipulation $validatedData =
-$request->validated(); $influencer->name = $validatedData['name'];
-$influencer->email = $validatedData['email']; $influencer->phone
-=$validatedData['phone']; $influencer->description =
-$validatedData['description']; if ($request->hasFile('image')) { $uploadPath =
-'uploads/influencer/'; $path = 'uploads/influencer/' . $influencer->image; if
-(File::exists($path)) { File::delete($path); } $file =
-$request->file(('image')); $ext = $file->getClientOriginalExtension(); $filename
-= time() . '.' . $ext; $file->move('uploads/influencer/', $filename);
-$influencer->image =$uploadPath.$filename; } $influencer->update(); return
-redirect('/admin/influencer')->with('message', 'influencer updated
-Successfully'); } }
+        $influencer->description = $validatedData['description'];
+
+        $uploadPath = 'uploads/influencer/';
+        if ($request->hasFile('image')) {
+            $file = $request->file(('image'));
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+            //current timestamp
+            $file->move('uploads/influencer/', $filename);
+            $influencer->image = $uploadPath . $filename;
+        }
+
+        $influencer->save();
+
+        return redirect('/admin/influencer')->with('message', 'influencer Added Successfully');
+    }
+    public function edit(Influencer $influencer)
+    {
+        return view('admin.influencer.edit', compact('influencer'));
+    }
+    public function update(InfluencerFormRequest $request, $influencer)
+    {
+        //dd($request->all());
+        $influencer = Influencer::findOrFail($influencer);
+        //retrieve and work with specific records from the database for further manipulation
+        $validatedData = $request->validated();
+        $influencer->name = $validatedData['name'];
+        $influencer->email = $validatedData['email'];
+        $influencer->phone =$validatedData['phone'];
+        $influencer->description = $validatedData['description'];
+        if ($request->hasFile('image')) {
+
+            $uploadPath = 'uploads/influencer/';
+
+            $path = 'uploads/influencer/' . $influencer->image;
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+            $file = $request->file(('image'));
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+            $file->move('uploads/influencer/', $filename);
+            $influencer->image =$uploadPath.$filename;
+
+
+        }
+
+
+        $influencer->update();
+        return redirect('/admin/influencer')->with('message', 'influencer updated Successfully');
+    }
+}
