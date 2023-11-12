b12525bda480aa20aad62ecce64fdf297b481d5a
islamatiyat01 <islamatiyat50@yahoo.com>
2023-11-12 17:30:54 +0300

    footer style

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
         {{-- footer --}}
         <footer>
-                <div class="d-flex flex-row ">
-        <div class="d-flex flex-column mb-3">
+                <div class="d-flex flex-row mb-1">
+        <div class="d-flex flex-column ">
                 <div class="p-2"><p>+962 79 060 2926</p></div>
                 <div class="p-2"><p>info@influhub.club</p></div>
         </div>
-        <div class="d-flex flex-column mb-3">
+        <div class="d-flex flex-column ">
                 <div class="p-2"><p>www.influhub.club</p></div>
                 <div class="p-2"><p>Amman - Dabouq</p></div>
         </div>
